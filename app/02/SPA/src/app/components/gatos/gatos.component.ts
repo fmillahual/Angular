efae42279d424774788b57d22b61609fbefde204
Fmillahual <fabian.millahual.m@gmail.com>
2019-01-04 16:09:03 -0400

    nav-bar
    Se agrega nav-bar

---
+++ app/02/SPA/src/app/components/gatos/gatos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gatos',
+  templateUrl: './gatos.component.html',
+  styles: []
+})
+export class GatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
