7a871055a25dc5234a5b6050ee0270d2c47c1e99
Fmillahual <fabian.millahual.m@gmail.com>
2019-01-15 15:55:19 -0300

    se agregan mas pipes

--- app/03/PIPES/src/app/app.component.ts
+++ app/03/PIPES/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   nombre:string = "Fabian";
+  nombre2:string = "dreYka Pru Pru 2 aÑos";
   arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
-
   PI = Math.PI;
   a = 0.234;
-
   salario = 1234.5;
+  gato = {
+    nombre: "Dreyka",
+    clave: "pruPru",
+    edad: 2,
+    direccion:{
+      calle:"san diego",
+      numero:"1473"
+      
+    }
+  };
+  video:string ="XII7af9SrLI";
+  activar:boolean = true;
+  promesa = new Promise( (resolve,reject )=>{
+    setTimeout( ()=>resolve('llego la data!'), 3500);
+  })
+
+
+  fecha = new Date();
 }
--- app/03/PIPES/src/app/app.module.ts
+++ app/03/PIPES/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { Capitalizado } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena-pipe.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Capitalizado,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:"en"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
